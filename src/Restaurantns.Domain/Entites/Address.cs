11d6a95bc47a4980e3ca0acda7a95581e5318e74
Hesham Elsayed <heesham749@gmail.com>
2025-09-20 23:29:43 +0300

    adding entities

---
+++ src/Restaurantns.Domain/Entites/Address.cs
@@ -0,0 +1,10 @@
+namespace Restaurantns.Domain.Entites;
+
+public class Address
+{
+	public string? City { get; set; }
+
+	public string? Street { get; set; }
+
+	public string? PostalCode { get; set; }
+}
---
+++ src/Restaurantns.Domain/Entites/Dish.cs
@@ -0,0 +1,13 @@
+namespace Restaurantns.Domain.Entites;
+
+public class Dish
+{
+	public int Id { get; set; }
+
+	public string Name { get; set; } = null!;
+
+	public string Description { get; set; } = null!;
+
+	public decimal Price { get; set; }
+
+}
