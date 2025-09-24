375ea54534a6b816a7c0d01d5d515a4161325201
Hesham Elsayed <heesham749@gmail.com>
2025-09-25 02:12:48 +0300

    adding pagedqueryValidator

---
+++ src/Restaurantns.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Restaurantns.Application.Restaurants.Queries.GetAllRestaurants;
+
+public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+{
+	public GetAllRestaurantsQueryValidator()
+	{
+		RuleFor(x => x.PageNumber)
+		.GreaterThanOrEqualTo(1);
+
+		RuleFor(x => x.PageSize)
+		.InclusiveBetween(1, 100);
+	}
+}
